d72b10fa967308587847d0c021b367740f3fe5c0
Keith Isham <kisham8899@gmail.com>
2019-11-13 12:12:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "pages" => "pages#index"
+  get "pages" => "pages#hello_world"
   # root '/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
