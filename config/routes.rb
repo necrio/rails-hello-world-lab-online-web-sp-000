8a47fcb1922264c8340d15072d37f60d77014a16
Keith Isham <kisham8899@gmail.com>
2019-11-13 12:06:08 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'hello_world' =>
+  get 'hello_world' => 'post#home'
   # root '/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
